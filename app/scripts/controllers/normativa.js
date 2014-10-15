a7ce24d07f7dd1c784d26c62b9f761a6b8631914
javier <javrod83@gmail.com>
2014-10-15 15:47:58 -0300

    minor css fixes

--- app/scripts/controllers/normativa.js
+++ app/scripts/controllers/normativa.js
@@ -9,9 +9,45 @@
  */
 angular.module('digestoApp')
   .controller('NormativaCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+
+
+     var state = "indice"  ;  // estados  "test" ,'indice' y "mock"
+
+    var url = {
+        mock : "http://dsdp.hcdn.gob.ar/digesto/json.txt",
+        test : "http://sparl-desa.hcdn.gob.ar:8080/exist/rest/digesto/index-json.xql",
+      indice : "http://dsdp.hcdn.gob.ar/digesto/indice.txt",
+
+    };
+
+
+    var vocales = {
+        a : '[aàáâãå]',
+        e : '[eèéêë]',
+        i : '[iìíîï]',
+        o : '[oòóôö]',
+        u : '[uùúûü]'
+    }
+
+    function replaceAll(str,find, replace) {
+          return str.replace(new RegExp(find, 'g'), replace);
+        }
+
+    function getRegex(str)
+        {
+            var out = str ;
+            for(var index in vocales) 
+                {
+                    out = replaceAll(out,index,vocales[index]);
+                      
+                }
+                 
+            return out ;
+        }
+
+
+
+
+
   });
