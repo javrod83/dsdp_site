08da169c9a9bb82a9d502441fa804097ce59944d
javier <javrod83@gmail.com>
2014-10-16 14:26:34 -0300

    algunos cambios

--- app/scripts/controllers/normativa.js
+++ app/scripts/controllers/normativa.js
@@ -71,27 +71,68 @@ angular.module('digestoApp')
 
         $scope.displayResult =     function ()
             {
-                console.log('displayResult');
-            $scope.resultado = "LOADING " ;
-            var xml = loadXMLDoc("xml/ley22990.xml");
-            var xsl = loadXMLDoc("scripts/xsl/norma_html_01.xsl");
-            // code for IE
-            if (window.ActiveXObject || xhttp.responseType == "msxml-document")
-              {
-                console.log('displayResult');
-              ex = xml.transformNode(xsl);
-              document.getElementById("example").innerHTML = ex;
-              }
-            // code for Chrome, Firefox, Opera, etc.
-            else if (document.implementation && document.implementation.createDocument)
-              {
-                 console.log('displayResult');
-                  var xsltProcessor = new XSLTProcessor();
-                  xsltProcessor.importStylesheet(xsl);
-                  console.log(xsltProcessor.transformToFragment(xml, document));
-                  document.getElementById("example").appendChild( xsltProcessor.transformToFragment(xml, document));
-              }
+                    console.log('displayResult');
+                    $scope.resultado = "LOADING " ;
+                    // var xml = loadXMLDoc("xml/ley22990.xml");
+                    // var xsl = loadXMLDoc("scripts/xsl/norma_html_01.xsl");
+                    // // code for IE
+                    // if (window.ActiveXObject || xhttp.responseType == "msxml-document")
+                    //   {
+                    //     console.log('displayResult');
+                    //   ex = xml.transformNode(xsl);
+                    //   document.getElementById("example").innerHTML = ex;
+                    //   }
+                    // // code for Chrome, Firefox, Opera, etc.
+                    // else if (document.implementation && document.implementation.createDocument)
+                    //   {
+                    //      console.log('displayResult');
+                    //       var xsltProcessor = new XSLTProcessor();
+                    //       xsltProcessor.importStylesheet(xsl);
+                    //       console.log(xsltProcessor.transformToFragment(xml, document));
+                    //       document.getElementById("example").appendChild( xsltProcessor.transformToFragment(xml, document));
+                    //   }
+
+                    transformOutput("xml/ley22990.xml","scripts/xsl/norma_html_01.xsl");
             }
+
+
+
+
+function loadXMLDocument (url) { 
+var httpRequest = new XMLHttpRequest(); 
+httpRequest.open('GET', url, false); 
+httpRequest.send(null); 
+
+return httpRequest.responseXML; 
+} 
+
+function createXMLDocument (rootName) { 
+var xmlDocument = document.implementation.createDocument('', rootName, null); 
+return xmlDocument; 
+} 
+
+
+function transformOutput(xmlinput, xslinput, parameternames, parameters)
+    { 
+        var xmlDocument    = loadXMLDocument(xmlinput); 
+        var xslDocument    = loadXMLDocument(xslinput); 
+        var resultDocument = createXMLDocument('whatever'); 
+        var xsltProcessor  = new XSLTProcessor(); 
+
+        if (xsltProcessor.transformDocument)
+            { 
+                xsltProcessor.transformDocument(xmlDocument, xslDocument, resultDocument, null); 
+
+                var s = new XMLSerializer(); 
+                var str = s.serializeToString(resultDocument); 
+
+                document.open(); 
+                document.write(str); 
+                document.close();
+                $scope.resultado = str; 
+            } 
+    }
+
 
 
   });
