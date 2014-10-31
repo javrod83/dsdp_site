53f96f77c08ca91abc0569964f6c8b013bbad8a1
javier <javrod83@gmail.com>
2014-10-31 16:33:28 -0300

    ultimos cambios

--- app/scripts/controllers/normativa.js
+++ app/scripts/controllers/normativa.js
@@ -10,23 +10,40 @@
 angular.module('digestoApp')
   .controller('NormativaCtrl',['$scope','ServiciosDeBusqueda',function ($scope,ServiciosDeBusqueda) {
 
-      function log(method,msg)
-        {
-          console.log('[NormativaCtrl]: '+method+' : '+msg);
-        }
+      var resultadosPorPagina = 10   ; 
+      var paginas             = []   ;
+      
+      $scope.resultadosTotales   =    '' ;
+      $scope.textoBusqueda       =    '' ; 
+      $scope.sugerencias         =   [ ] ;
+      $scope.respuestas          =   [ ] ;
+      $scope.numeroDePaginas     =   [ ] ; 
+      $scope.preloader           = false ; 
+      $scope.showResult          = false ;
 
 
-      $scope.textoBusqueda = '' ; 
-      $scope.preloader     = false;
-      $scope.sugerencias   = [];
-      $scope.respuestas    = [];
-      $scope.showResult    = false
-      $scope.paginas       = [ 1,2,3,4,5 ] ; 
+    function log(method,msg)
+      {
+        console.log('[NormativaCtrl]: '+method+' : '+msg);
+      }
 
+    function renombrarPropiedadTexto(resultados )
+      {
+          //modificar el atributo #texto por texto 
+          $(resultados).each(function(indice,elemento){
+            elemento.texto = elemento['#texto'];
+            delete elemento['#texto'];
+          });
+      }
 
+    $scope.mostrarPagina = function (numeroDePagina)
+      {
+        log('mostrarPagina','change to page '+numeroDePagina);
+        $scope.respuestas = $scope.paginas[numeroDePagina]; 
+      };
+    
     $scope.buscarSugerencias = function ()
       {
-
         log('buscarSugerencias','buscando sugerencias'); 
         $scope.sugerencias = ['buscando sugerencias ... '];
         ServiciosDeBusqueda.getSugerenciasBusqueda($scope.textoBusqueda).then(function(data){
@@ -46,48 +63,68 @@ angular.module('digestoApp')
         });
       };
 
-    $scope.sugerenciaSeleccionada = function(sugerencia){
-        log('sugerenciaSeleccionada','iniciando'); 
-        $scope.textoBusqueda = sugerencia ;
-        $scope.buscar() ; 
-    }; 
+    $scope.sugerenciaSeleccionada = function(sugerencia)
+      {
+          log('sugerenciaSeleccionada','iniciando'); 
+          $scope.textoBusqueda = sugerencia ;
+          $scope.buscar() ; 
+      }; 
 
-  $scope.buscar = function()
-  	{
-        log('buscar','buscando '+$scope.textoBusqueda); 
-    		$scope.preloader = true;
-        $scope.sugerencias = [] ; 
-    		ServiciosDeBusqueda.getNormaPorQuery($scope.textoBusqueda).then(function(data){
-    			//busqueda exitosa 
+    $scope.buscar = function()
+    	{
+          log('buscar','buscando '+$scope.textoBusqueda); 
+      		$scope.preloader = true;
+          $scope.sugerencias = [] ; 
+          $scope.resultadosTotales = '' ; 
 
-    			$scope.preloader = false;
-          $scope.showResult = true ;
-          if(data.length !== 0 )
-            {
-              $scope.respuestas = cleanResultados(data[0].Norma) ; 
-            }
-          else
-            {
-            }
+      		ServiciosDeBusqueda.getNormaPorQuery($scope.textoBusqueda).then(function(data){
+      		    //busqueda exitosa 
+        			$scope.preloader = false;
+              procesarResultados(data) ; 
+              //log('buscar','se encontraron '+$scope.respuestas.length); 
+      		},function(err){
+      			//eror en la bsuqueda
+      			$scope.preloader = false;
+      		});
+    	}; 
 
-            log('buscar','se encontraron '+$scope.respuestas.length); 
-    		},function(err){
-    			//eror en la bsuqueda
-    			$scope.preloader = false;
+    function procesarResultados ( data )
+        {
+            //detener loader 
+            $scope.showResult = true ;
+            if(data.length !== 0 )
+                {
+                    var resultados = data[0].Norma;
+                    renombrarPropiedadTexto(resultados);
+                    $scope.resultadosTotales = resultados.length ; 
 
-    		});
+                    //paginar resultados ? ? 
+                    if (resultados.length > resultadosPorPagina)
+                        {
+                            $scope.paginas         = [] ; 
+                            $scope.numeroDePaginas = [] ; 
 
-  	}; 
+                            var cotaMenor = 0 ; 
+                            var cotaMayor = 0 ; 
 
+                            for (var i = 1; i < (Math.ceil(resultados.length/resultadosPorPagina)); i++)
+                                {
+                                    cotaMenor = ( i - 1 ) * resultadosPorPagina ; 
+                                    cotaMayor =   Math.min(( cotaMenor + (resultadosPorPagina - 1 ) ),resultados.length-1)    ; 
+                                    $scope.paginas[i]=resultados.slice(cotaMenor,cotaMayor) ;
+                                    $scope.numeroDePaginas.push(i);
+                                }
 
-    function cleanResultados ( resultados )
-    {
-      $(resultados).each(function(indice,elemento){
-        elemento.texto = elemento['#texto'];
-        delete elemento['#texto'];
-      });
-
-      return  resultados      
-    }
-
+                            $scope.respuestas =  $scope.paginas[1];
+                        }
+                    else
+                        {
+                            $scope.respuestas = resultados;
+                        }
+                }
+            else
+                {
+                  // mostrar mensaje de que no hay resultados 
+                }      
+        }
   }]);
