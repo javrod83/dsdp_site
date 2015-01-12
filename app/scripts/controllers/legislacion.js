f0eef0833b8ec524b6113b7ac1cc6ffbbe9a190b
bel <belen.ruibal.dip@gmail.com>
2015-01-12 14:31:41 -0300

    normativa minor changes

--- app/scripts/controllers/legislacion.js
+++ app/scripts/controllers/legislacion.js
@@ -8,7 +8,7 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('LegislacionctrlCtrl', function ($scope) {
+  .controller('LegislacionCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
